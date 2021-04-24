fc071f116e53c1d5dc990938eebc7179d3c9940f
Delph-SG <73239197+Delph-SG@users.noreply.github.com>
2021-04-25 01:41:54 +0200

    upload activities
    créations des activités

---
+++ convert/app/src/main/java/com/example/convert/convert_activities/Currency.java
@@ -0,0 +1,15 @@
+package com.example.convert.convert_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.convert.R;
+
+public class Currency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
+    }
+}
